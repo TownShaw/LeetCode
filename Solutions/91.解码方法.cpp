47f7b42769fb8a59f11147384f5fbd59e59c1ab4
XT <773540317@qq.com>
2021-01-26 17:19:09 +0800

    2021/1/26

---
+++ Solutions/91.解码方法.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=91 lang=cpp
+ *
+ * [91] 解码方法
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> numofDecode;
+
+    int numDecodings(string s)
+    {
+        numofDecode.clear();
+        for (int i = 0; i < s.size(); i++)
+        {
+            numofDecode.push_back(0);
+        }
+/*        int Edge = s.size() - 1;
+        if (s[Edge] != '0')
+        {
+            numofDecode[Edge] = 1;
+        }
+        else
+        {
+            if (s[Edge - 1] != '1' && s[Edge - 1] != '2')
+            {
+                return 0;
+            }
+            else
+            {
+                numofDecode[Edge - 1] = 1;
+            }
+        }*/
+        int Kind_Num = Decode(s, 0);
+        return Kind_Num;
+    }
+
+    bool isCode(string &s, int begin, int length)
+    {   // s[begin] --- s[begin + length - 1] 是否为一个编码
+        if (s[begin] == '0' || begin + length > s.size())
+        {
+            return false;
+        }
+        int num = 0;
+        for (int i = begin; i < begin + length; i++)
+        {
+            num = num * 10 + (s[i] - '0');
+        }
+        if (num <= 26)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    int Decode(string &s, int begin)
+    {
+        if (begin >= s.size())
+        {
+            return 0;
+        }
+        if (numofDecode[begin] != 0)
+        {
+            return numofDecode[begin];
+        }
+        if (isCode(s, begin, 1))
+        {
+            numofDecode[begin] += Decode(s, begin + 1);
+            if (begin + 1 == s.size())  //本身就是一种解码方式
+            {
+                numofDecode[begin]++;
+            }
+        }
+        if (isCode(s, begin, 2))
+        {
+            numofDecode[begin] += Decode(s, begin + 2);
+            if (begin + 2 == s.size())  //本身就是一种解码方式
+            {
+                numofDecode[begin]++;
+            }
+        }
+        return numofDecode[begin];
+    }
+};
+// @lc code=end
+
