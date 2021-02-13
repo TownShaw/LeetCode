c18ef86359f17565f67092339289fa95e43f0afe
XT <773540317@qq.com>
2021-02-13 22:25:48 +0800

    2021/2/13

---
+++ Solutions/279.完全平方数.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=279 lang=cpp
+ *
+ * [279] 完全平方数
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+class Solution {
+public:
+    int numSquares(int n)
+    {
+        std::vector<int> DP(n + 1, 0);
+        return MinSquare(n, DP);
+    }
+
+    int MinSquare(int n, std::vector<int> &DP)  //返回数字 n 的 Min_num_Square
+    {
+        if (DP[n] != 0)
+        {
+            return DP[n];
+        }
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            DP[n] = 1;
+            return 1;
+        }
+        int less_Num = int(sqrt(n));    //平方小于 n 的最大整数, less_Num
+        int Min = 10000;
+        while (less_Num >= 1)
+        {
+            int count = MinSquare(n - less_Num * less_Num, DP) + 1;
+            Min = (Min < count) ? Min : count;
+            less_Num--;
+        }
+        DP[n] = Min;
+        return Min;
+    }
+};
+// @lc code=end
+
